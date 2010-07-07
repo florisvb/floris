4a42da76236646a8213cea314a80ebc818f1c821
Floris van Breugel <floris@sericomyia-analysis.(none)>
2010-07-06 21:42:11 -0700

    added reconstructor stuff

--- plot_calibration.py
+++ plot_calibration.py
@@ -81,7 +81,7 @@ class Calibration:
         
     def write_to_xml(self, pmat, filename=None):
         if filename is None:
-            filename = time.strftime("ptf_calibration_xml_%Y%m%d_%H%M%S.xml",time.localtime())
+            filename = time.strftime("ptf_calibration_xml_%Y%m%d_%H%M%S.txt",time.localtime())
         print 'pmat: '
         print pmat
         K,R,t = camera_math.decomp(pmat)
@@ -90,9 +90,34 @@ class Calibration:
         Knew = np.eye(3)
         Rnew = R
         Rt = np.hstack((R,t))
-        Pnew = np.dot( Knew, Rt)
+        Kscaled = K / K[2,2]
+        print 'kscaled: '
+        print Kscaled
+        Pnew = np.dot( Kscaled, Rt)
         self.SingleCameraCalibration = reconstruct.SingleCameraCalibration_from_basic_pmat(Pnew, cam_id='ptf', res=[1000,1000])
-        self.SingleCameraCalibration.to_file(filename)
+        #self.SingleCameraCalibration.to_file(filename)
+        #reconstruct.pretty_dump(self.SingleCameraCalibration)
+        # now load Reconstructor
+        self.Reconstructor = reconstruct.Reconstructor([self.SingleCameraCalibration])
+        return
+        
+    def to_motor_coords(self, obj_pos):
+        # takes 3D object as input, returns the three corresponding motor positions
+        # back out desired motor positions
+        [u,v] = self.Reconstructor.find2d('ptf',obj_pos)
+        pan_pos = np.arctan2(u,1) # focal length of 1, arbitrary
+        tilt_pos = np.arctan2(v,1)
+        focus_pos = self.focus.calc_focus(obj_pos)
+        motor_coords = [pan_pos, tilt_pos, focus_pos]
+        #print motor_coords
+        
+        return motor_coords
+        
+        
+    #def find_R(self):
+    
+        # for each 3D point, find the rotation from the 3D flydra vector, 
+        
         
 if __name__=='__main__':
     cal = Calibration()
@@ -102,7 +127,12 @@ if __name__=='__main__':
     cal.plot()
     
     Pnew = camera_math.replace_camera_center(cal.Mhat, cal.focus.camera_center)
-    cal.write_to_xml(Pnew)
+    cal.write_to_xml(cal.Mhat)
     
-    
+    for i in range(np.shape(cal.data)[0]):
+        
+        obj_pos = cal.data[i,3:6]
+        reprojected_motor_pos = cal.to_motor_coords(obj_pos)
+        real_motor_pos = cal.data[i,0:3]
+        print np.abs( reprojected_motor_pos - real_motor_pos )
     
