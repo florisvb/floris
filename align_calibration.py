fixed zaxis calculation

align_calibration.py
@@ -11,20 +11,20 @@ import flydra.reconstruct as reconstruct
 
 ########### user entered values ######################################
 
-orig_cal_file = '/home/floris/data/calibrations/20100609_arena/20100609_scaled.xml'
-new_xml_file = '/home/floris/data/calibrations/20100609_arena/20100609_postaligned.xml'
+orig_cal_file = '/home/floris/data/calibrations/20100803_arena/20100803_cal_scaled.xml'
+new_xml_file = '/home/floris/data/calibrations/20100803_arena/20100803_cal_aligned.xml'
 
 # center: enter the 3d point that you would like to be the center of the new calibration
-center = np.array([-0.04108834, -0.0129954 , -0.22327666])
+center = np.array([-0.00160242,  0.0196297 ,  0.15449429])
 
 # for scale: enter two points, s1 and s2. the distance between these two points will be scaled to equal the desired size
-s1 = np.array([-0.04124752, 0.00477603, -0.2220248])
-s2 = np.array([ -0.03948431, -0.03066134, -0.22075723])
-desired_size = .01912
+s1 = np.array([ 0.05066727, -0.01933785, -0.02735403])
+s2 = np.array([ 0.04925564,  0.09637216, -0.03131262])
+desired_size = 0.1
 
 # zaxis: enter two points along the desired zaxis
-p1 = np.array([-0.03357589, -0.0270953 , -0.17019208])
-p2 = np.array([ 0.01346261, -0.03384385, 0.15798934])
+p1 = np.array([0.00204226,  0.00212723,  0.11089473])
+p2 = np.array([  0.05369163, -0.00499771, -0.09935711])
 
 #######################################################################
 
@@ -33,7 +33,7 @@ p2 = np.array([ 0.01346261, -0.03384385, 0.15798934])
 size = scipy.linalg.norm(s2-s1)
 scale = desired_size/size
 
-nz = (p2 - p2) / scipy.linalg.norm((p1 - p2))
+nz = (p1 - p2) / scipy.linalg.norm((p1 - p2))
 z_axis = np.array([0,0,1])
 
 # source: wikipedia, rotation matrices
@@ -51,6 +51,8 @@ Rn = np.array([ [ rot_axis[0]**2+(1-rot_axis[0]**2)*np.cos(rot_angle),
 
 center_scaled_rotated = np.dot(Rn, center) * scale
 M = align.build_xform(scale,Rn,-1*center_scaled_rotated)
+
+print nz
 
 srcR = reconstruct.Reconstructor(cal_source=orig_cal_file)
 alignedR = srcR.get_aligned_copy(M)